17916af4ef1fd1ff5a754437eab9936dc565131a
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2026-04-01 17:16:27 +0200

    [Add] Sprite walk + demo script

---
+++ demo.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#python display_1led.py  
+python waterfall.py  
+python dropfall.py  
+python bouncingball.py  
+python starfield.py  
+python galaxy.py       
+python fireworks.py
+python fire.py          
+python cube3d.py     
+python sandfall.py  
+
+python lifegame.py
+python 2panelled.py     
+python 1ball2panel.py
+python fallingletters.py  
